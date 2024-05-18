ab112103fda16439c6a99bed83d2d5860fbdb322
DoubleMa <com.doublema@gmail.com>
2024-05-18 09:35:37 +0300

    changed the version

--- src/DoubleQoL.cs
+++ src/DoubleQoL.cs
@@ -10,7 +10,7 @@ using System;
 namespace DoubleQoL {
 
     public sealed class DoubleQoL : IMod {
-        public static Version ModVersion = new Version(1, 6, 1);
+        public static Version ModVersion = new Version(1, 6, 2);
         public string Name => "DoubleQoL";
         public int Version => 1;
         public bool IsUiOnly => false;
